9f10e3fdcf51162ba234f38e3b36ac1cffe0729f
unknown <wilmer_11_90@hotmail.com>
2026-07-08 02:50:53 -0500

    docs: add project library

---
+++ account-service/account-service/src/main/java/com/banking/accountservice/config/OpenApiConfig.java
@@ -0,0 +1,27 @@
+package com.banking.accountservice.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class OpenApiConfig {
+
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+
+        return new OpenAPI()
+                .info(
+                        new Info()
+                                .title("Account Service API")
+                                .version("1.0")
+                                .description(
+                                        "API for managing bank accounts"
+                                )
+                );
+    }
+}
